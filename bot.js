12c03fe92f63e1585c94e3b15d73cfb0b610f51a
Colin O'Kain <colinok2001@aol.com>
2020-06-17 22:49:00 -0500

    test

--- bot.js
+++ bot.js
@@ -61,7 +61,7 @@ disClient.on('message', msg => {
                 if(            
                     twilioClient.calls
                         .create({
-                            url: ('https://fishsaucey.com/callmessages/' + msg.author.username + 'call.xml'),
+                            url: ('http://fishsaucey.com/callmessages/' + msg.author.username + 'call.xml'),
                             to: '+1' + num,
                             from: '+12019077471'
                         })
