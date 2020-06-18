1e5939742d75751e194cf0b64515ee39fcf82feb
Colin O'Kain <colinok2001@aol.com>
2020-06-17 22:43:29 -0500

    test

--- bot.js
+++ bot.js
@@ -59,7 +59,9 @@ disClient.on('message', msg => {
                 createXML(msg.author.username, getMsg(args, 1));
                 const { promisify } = require('util');
                 const sleep = promisify(setTimeout);
-                sleep(50000);
+                sleep(10000).then(() => {
+
+                });
                 console.log('fishsaucey.com/callmessages/' + msg.author.username + 'call.xml');
                 if(            
                     twilioClient.calls
