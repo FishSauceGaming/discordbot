9a35731771f4a6c9816df4aa3a0f53e24b896246
Colin OKain <colinok2001@gmail.com>
2020-06-20 01:07:09 -0500

    test

--- bot.js
+++ bot.js
@@ -151,7 +151,7 @@ function getInput(data) {
 function createLinkXML(user, link) {
     link = link.replace("https", "http");
     var fs = require('fs');
-    fs.writeFile('/var/www/html/callmessages/' + user + 'call.xml', '<Response>\n\t<Stream url="' + link + '"></Stream>\n</Response>', function (err) {
+    fs.writeFile('/var/www/html/callmessages/' + user + 'call.xml', '<Response>\n\t<Stream url="' + link + '">\n</Response>', function (err) {
         if (err) throw err;
         console.log('Saved!');
     });
