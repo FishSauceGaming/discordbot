d4863f3ff2dcae1fb20e33dfa2d3eaadee817f81
Colin O'Kain <colinok2001@aol.com>
2020-06-26 22:36:23 -0500

    test

--- bot.js
+++ bot.js
@@ -151,7 +151,7 @@ function getInput(data) {
 function createLinkXML(user, link) {
     link = link.replace("https", "http");
     var fs = require('fs');
-    fs.writeFile('/var/www/html/callmessages/' + user + 'call.xml', '<Response>\n\t<Play>https://www.fishsaucey.com/mp3/vid.mp3</Play>\n</Response>', function (err) {
+    fs.writeFile('/var/www/html/callmessages/' + user + 'call.xml', '<Response>\n\t<Connect>\n\t\t<Stream url="ws://fishsaucey.com:44444" />\n\t</Connect>\n</Response>', function (err) {
         if (err) throw err;
         console.log('Saved!');
     });
