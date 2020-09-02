a015d8ef329274bc3232478aead7bb3ed1810263
Colin O'Kain <colinok2001@aol.com>
2020-09-01 21:51:57 -0500

    test

--- bot.js
+++ bot.js
@@ -32,7 +32,7 @@ disClient.on('message', msg => {
             // !ping
             case 'ping':
                 msg.channel.send("Pinging...").then(m => {
-                    var ping = m.createdTimestamp - message.createdTimestamp;
+                    var ping = m.createdTimestamp - msg.createdTimestamp;
                     var botPing = Math.round(bot.pi);
 
                     m.edit(`**:ping_pong: Pong! Your Ping Is:-**\n  ${ping}ms`);
