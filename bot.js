6b08b0bc1c81d8c0f34d2957067b55234305351a
Colin O'Kain <colinok2001@aol.com>
2020-09-08 22:39:19 -0500

    test

--- bot.js
+++ bot.js
@@ -172,6 +172,12 @@ async function getRedditPost(msg, sub) {
                 };
                 msg.reply({ embed: testembed });
                 msg.channel.send('||' + image.data.url + '||');
+                msg.channel.send({
+                    files: [{
+                        attachment: image.data.url,
+                        name: "SPOILER_FILE.jpg"
+                    }]
+                });
                 return;
             } else {
                 var testembed = {
