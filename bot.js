9158177c0cd284b2b802fc76ddf0ae7f7885405d
Colin O'Kain <colinok2001@aol.com>
2020-10-16 23:29:05 -0500

    test

--- bot.js
+++ bot.js
@@ -211,7 +211,7 @@ function readContactList(user, id, msg) {
     var i = 1;
     for (property in parsed) {
         stri = toString(i);
-        console.log(stri + ' ' + ntm[stri])
+        console.log(stri)
         body = body.concat(ntm[stri] + ": " + parsed[property] + '\n');
         i++;
     }
