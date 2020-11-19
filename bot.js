9c4c16c4850c571104e4ac82cd3d1a354e0ee319
Colin O'Kain <colinok2001@aol.com>
2020-11-18 20:36:30 -0600

    test

--- bot.js
+++ bot.js
@@ -205,7 +205,7 @@ disClient.on('message', msg => {
 
 function cron(team, monthVar, dayVar, hourVar, minuteVar, msg1, msg) {
     var varYear = new Date().getFullYear();
-    var varDate = new Date(varYear, monthVar - 1, dayVar, hourVar, minuteVar + 1, 0);
+    var varDate = new Date(varYear, monthVar - 1, dayVar-1, hourVar+1, minuteVar + 1, 0);
     console.log(varDate);
     msg.reply('scheduled...');
     schedule.scheduleJob(varDate, function () {
