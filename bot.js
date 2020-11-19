08bd5cf4a9b79e5a1f5949f031a0350067c3baac
Colin O'Kain <colinok2001@aol.com>
2020-11-18 19:40:45 -0600

    test

--- bot.js
+++ bot.js
@@ -205,7 +205,7 @@ disClient.on('message', msg => {
 
 function cron(team, monthVar, dayVar, hourVar, minuteVar, msg1, msg) {
     msg.reply('scheduled...');
-    schedule.scheduleJob({ hour: 19, minute: 40, dayOfWeek:4 }, function () {
+    schedule.scheduleJob({ hour: 19, minute: 42, day: 18 }, function () {
         msg.reply("works")
     });
 }
